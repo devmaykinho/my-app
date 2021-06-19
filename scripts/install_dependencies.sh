7ae4d7b06da9521fe4255ce4934285059362fa01
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2021-06-19 00:05:57 -0300

    first

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo installing application with npm
+cd /my-app
+sudo npm install
---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo starting server
+
+# here we just use npm to run the build
+cd /my-app
+echo building application...
+sudo npm start
